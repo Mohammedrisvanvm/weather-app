ccbdcb10c1fd3a4abc5371cd05ef113def08cea4
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2024-02-14 16:29:23 +0530

    data fetched using api and displayed

--- src/Components/WeatherApp/WeatherApp.tsx
+++ src/Components/WeatherApp/WeatherApp.tsx
@@ -8,13 +8,44 @@ import humidity_icon from "../../assets/humidity.png";
 import rain_icon from "../../assets/rain.png";
 import snow_icon from "../../assets/snow.png";
 import wind_icon from "../../assets/wind.png";
+import { WeatherData } from "../../interface/weatherApi";
 
 const WeatherApp: React.FC = () => {
+  const key = `a390c0c93e2b589d787363d24a92cff3`;
+  const search = async () => {
+    const element = document.getElementsByClassName(
+      "cityinput"
+    ) as HTMLCollectionOf<HTMLInputElement>;
+
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${key}`;
+
+    let response = (await (await fetch(url)).json()) as WeatherData;
+
+    const temperature = document.getElementsByClassName(
+      "weather-temp"
+    ) as HTMLCollectionOf<HTMLInputElement>;
+    const humidity = document.getElementsByClassName(
+      "humidity-percentage"
+    ) as HTMLCollectionOf<HTMLInputElement>;
+    const windspeed = document.getElementsByClassName(
+      "wind-speed"
+    ) as HTMLCollectionOf<HTMLInputElement>;
+    const location = document.getElementsByClassName(
+      "weather-location"
+    ) as HTMLCollectionOf<HTMLInputElement>;
+    temperature[0].innerHTML = response.main.temp;
+    humidity[0].innerHTML = response.main.humidity;
+    windspeed[0].innerHTML = response.wind.speed;
+    location[0].innerHTML = response.name;
+  };
   return (
     <div className="container">
       <div className="top-bar">
         <input type="text" className="cityinput" placeholder="Search" />
-        <div className="search-icon">
+        <div className="search-icon" onClick={() => search()}>
           <img src={search_icon} alt="" />
         </div>
       </div>
